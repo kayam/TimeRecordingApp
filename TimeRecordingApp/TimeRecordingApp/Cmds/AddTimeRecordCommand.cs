d6b820312a89e95de1acaac0ce68c3db86af8b12
kayam <mkaya@outlook.de>
2025-06-16 18:43:33 +0200

    Korrektur Button Neu

--- TimeRecordingApp/TimeRecordingApp/Cmds/AddTimeRecordCommand.cs
+++ TimeRecordingApp/TimeRecordingApp/Cmds/AddTimeRecordCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Data;
 using TimeRecording.Dal.Entities;
 
 namespace TimeRecordingApp.Cmds
@@ -19,21 +20,20 @@ namespace TimeRecordingApp.Cmds
 
         public Activities MDefaultActivity { get; }
 
-        public override bool CanExecute(object parameter) => parameter is ObservableCollection<TimeRecordings>;
+        public override bool CanExecute(object parameter) => true;
 
         public override void Execute(object parameter)
         {
-            if ( ! (parameter is ObservableCollection<TimeRecordings> timeRecords)) return;
-            //var maxCount = timeRecords.Max(x => x.Id);
-            var newItem = new TimeRecordings { Activity = MDefaultActivity, Date = DateTime.Today, StartTime = DateTime.Now.TimeOfDay, EndTime = DateTime.Now.TimeOfDay.Add(TimeSpan.FromHours(1)), Note = "", MIsChanged = true };
-            timeRecords.Add(newItem);
+            if (parameter is ListCollectionView view && view.SourceCollection is ObservableCollection<TimeRecordings> timeRecords) { 
+                var newItem = new TimeRecordings { Activity = MDefaultActivity, Date = DateTime.Today, StartTime = DateTime.Now.TimeOfDay, EndTime = DateTime.Now.TimeOfDay.Add(TimeSpan.FromHours(1)), Note = "", MIsChanged = true };
+                timeRecords.Add(newItem);
              
-            MMainWin.gridTimeRecords.Focus();
-            DataGridCellInfo cellInfo = new DataGridCellInfo(newItem, MMainWin.gridTimeRecords.Columns[0]);
-            MMainWin.gridTimeRecords.CurrentCell = cellInfo;
-            MMainWin.gridTimeRecords.ScrollIntoView (newItem);
-            MMainWin.gridTimeRecords.BeginEdit();
-            
+                MMainWin.gridTimeRecords.Focus();
+                DataGridCellInfo cellInfo = new DataGridCellInfo(newItem, MMainWin.gridTimeRecords.Columns[0]);
+                MMainWin.gridTimeRecords.CurrentCell = cellInfo;
+                MMainWin.gridTimeRecords.ScrollIntoView (newItem);
+                MMainWin.gridTimeRecords.BeginEdit();
+            }
         }
 
     }
